0bebda06dfbde4f11675458563b1d628a3d1fba2
AshwagAlharthi <Ashwag.alharthi@hotmail.com>
2024-09-12 14:15:02 +0300

    solved

---
+++ main.js
@@ -0,0 +1,26 @@
+let stopButton = document.getElementById("stopButton");
+let slowButton = document.getElementById("slowButton");
+let goButton = document.getElementById("goButton");
+
+let stopLight = document.getElementById("stopLight");
+let slowLight = document.getElementById("slowLight");
+let goLight = document.getElementById("goLight");
+
+stopButton.addEventListener("click", ()=>{
+    stopLight.style.backgroundColor = "red";
+    slowLight.style.backgroundColor = "";
+    goLight.style.backgroundColor = "";
+
+})
+
+slowButton.addEventListener("click", ()=>{
+    stopLight.style.backgroundColor = "";
+    slowLight.style.backgroundColor = "orange";
+    goLight.style.backgroundColor = "";
+})
+
+goButton.addEventListener("click", ()=>{
+    stopLight.style.backgroundColor = "";
+    slowLight.style.backgroundColor = "";
+    goLight.style.backgroundColor = "green";
+})
